6ab1e2ed4aac9f8fbe7a516373e649cd1f33f56f
Devansh Tiwari <120082911+Devansh1534@users.noreply.github.com>
2024-07-31 19:03:01 +0530

    Time: 636 ms (15.9%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 SELECT name
 FROM Customer
-WHERE  referee_id not exists (select* from Customer where refree_id not exists (select* from Customer where refree_id = 2))
+WHERE referee_id != 2 or referee_id is null
