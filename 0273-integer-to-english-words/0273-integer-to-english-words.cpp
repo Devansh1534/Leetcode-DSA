858a1e7dc4f9c8fde981e102303c6ccbc99eb4fd
Devansh Tiwari <120082911+Devansh1534@users.noreply.github.com>
2024-08-07 11:59:41 +0530

    Time: 4 ms (49.19%), Space: 12.5 MB (16.4%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -14,34 +14,25 @@ public:
 
     string numberToWords(int num) {
         if (num == 0) return "Zero";
-
-        string result;
-        int index = 0;
-
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                result = helper(num % 1000) + thousands[index] + " " + result;
-            }
-            num /= 1000;
-            index++;
-        }
-
-        while (!result.empty() && result.back() == ' ') {
-            result.pop_back();
-        }
-
-        return result;
+        return helper(num);
     }
 
+private:
     string helper(int num) {
-        if (num == 0) {
-            return "";
-        } else if (num < 20) {
-            return belowTwenty[num] + " ";
+        if (num < 20) {
+            return belowTwenty[num];
         } else if (num < 100) {
-            return tens[num / 10] + " " + helper(num % 10);
+            return tens[num / 10] + (num % 10 ? " " + helper(num % 10) : "");
+        } else if (num < 1000) {
+            return belowTwenty[num / 100] + " Hundred" + (num % 100 ? " " + helper(num % 100) : "");
         } else {
-            return belowTwenty[num / 100] + " Hundred " + helper(num % 100);
+            for (int i = 3; i >= 0; --i) {
+                int unit = pow(1000, i);
+                if (num >= unit) {
+                    return helper(num / unit) + " " + thousands[i] + (num % unit ? " " + helper(num % unit) : "");
+                }
+            }
         }
+        return "";
     }
 };
