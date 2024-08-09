58b2aa38271a928720b9b1f3f7a4624e8014f399
Devansh Tiwari <120082911+Devansh1534@users.noreply.github.com>
2024-08-09 21:14:25 +0530

    Time: 397 ms (68.22%), Space: 174.7 MB (72.26%) - LeetHub

---
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/2568-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    long long dfs(int node, int parent, vector<vector<int>>& adj, int seats, long long& fuel) {
+        int representatives = 1;
+        
+        for (int neighbor : adj[node]) {
+            if (neighbor != parent) {
+                representatives += dfs(neighbor, node, adj, seats, fuel);
+            }
+        }
+
+        if (node != 0) { // If it's not the capital
+            fuel += (representatives + seats - 1) / seats;
+        }
+
+        return representatives;
+    }
+    
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size() + 1;
+        vector<vector<int>> adj(n);
+        
+        for (auto& road : roads) {
+            adj[road[0]].push_back(road[1]);
+            adj[road[1]].push_back(road[0]);
+        }
+        
+        long long fuel = 0;
+        dfs(0, -1, adj, seats, fuel);
+        return fuel;
+    }
+};
