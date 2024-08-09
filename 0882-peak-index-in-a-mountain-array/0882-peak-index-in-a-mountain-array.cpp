edc424c49ebe7a868dd0b346700377d773f03790
Devansh Tiwari <120082911+Devansh1534@users.noreply.github.com>
2024-08-09 21:19:22 +0530

    Time: 65 ms (97.37%), Space: 62.4 MB (25.01%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+};
